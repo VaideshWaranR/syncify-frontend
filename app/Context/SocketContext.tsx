8d2a8a5da3afdd455c78d2c56739cbd4004bfc65
VaideshWaranR <vaiv64259@gmail.com>
2025-09-06 12:21:17 +0530

    socket wss

--- app/Context/SocketContext.tsx
+++ app/Context/SocketContext.tsx
@@ -5,7 +5,11 @@ import { io, Socket } from "socket.io-client";
 const SocketContext = createContext<Socket | null>(null);
 
 export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
-  const [socket] = useState(() => io("https://syncify-backend-yvq1.onrender.com/10000"));
+const [socket] = useState(() =>
+  io("https://syncify-backend-yvq1.onrender.com", {
+    transports: ["websocket"],
+  })
+);
 
   useEffect(() => {
     socket.connect();
